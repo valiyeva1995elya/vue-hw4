639928fed911aff36d5e455b96bbacfb05b4fb5a
ValiyevaElya <valiyeva1995elya>
2023-02-02 23:08:07 +0600

    hw4

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Post from "./components/Post.vue"
+
+</script>
+
+<template>
+  <Post/>
+</template>
+
+<style>
+  
+  
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+    data() {
+        return {
+            images: [],
+            image: 0,
+            src: ""
+        }
+    },
+    methods: {
+        addImg() {
+            if (this.src == "") {
+                return
+            }
+            let newBlock = this.src
+            this.images.push(newBlock)
+            this.src = ""
+        },
+
+
+
+    }
+}
+
+
+</script>
+
+<template>
+    <main>
+        <div>
+            <input type="text" placeholder="URL" v-model="src">
+            
+            <button @click="addImg">Добавить</button>
+        </div>
+
+        <button v-for="(n, i) in images" @click="image = i">
+            Картинка №{{ i + 1 }}
+        </button>
+
+        <transition name="image" mode="out-in">
+            <img :key="image" :src="images[image]">
+        </transition>
+
+
+    </main>
+</template>
+
+<style scoped>
+main{
+    display: flex;
+    flex-direction: column;
+}
+.image-enter-active,
+.image-leave-active {
+  transition: opacity .3s;
+}
+
+.image-enter,
+.image-leave-to {
+  opacity: 0;
+}
+</style>
